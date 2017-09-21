11948eb48a1fe7481ede65fb09db351ea99767c7
John Lynn <john@silferein.com>
2017-09-20 20:31:00 -0700

    Downgrade to bootstrap 3.x, fleshout nav bar.

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<p>Lookit me, ma! I'm a header!</p>`
+  templateUrl: 'header.component.html'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent { }
